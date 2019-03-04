f80a54cb9edc32611f166aa0dc6616b114446634
Nicolas Huguenin <nichuguen@gmail.com>
2019-03-04 18:13:50 +0100

    analysis and fitting of new model

--- real_build.sh
+++ real_build.sh
@@ -6,3 +6,5 @@ biber $NAME
 makeglossaries $NAME
 pdflatex -shell-escape -interaction=nonstopmode -file-line-error $NAME
 pdflatex -shell-escape -interaction=nonstopmode -file-line-error $NAME
+
+cp thesis.pdf NHuguenin_TM_2018-2019_S1.PDF
